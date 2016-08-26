9e35b0be8888502e9f51c07957f923884ea92c58
caocaoer <caocaoer1019@163.com>
2016-08-26 16:19:04 +0800

    update

--- basic-demo/dist/all.js
+++ basic-demo/dist/all.js
@@ -4,3 +4,4 @@ var arrs = [1, 2, 3, 4, 5];
 arrs.map(function (n) {
   return n;
 });
+//# sourceMappingURL=all.js.map
--- basic-demo/gulpfile.js
+++ basic-demo/gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
     babel = require('gulp-babel');
 
 gulp.task('default', function(){
     gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
     .pipe(babel())
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
 });
